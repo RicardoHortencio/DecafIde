962168ca96275828af048182848f6c751afa2396
Paulo Chang <pigenio@gmail.com>
2014-04-21 07:07:10 -0600

    Added untracked files.

---
+++ DecafIde/LipBased/BaseClasses/SymbolTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecafIde.LipBased
+{
+    public enum SymbolType
+    {
+        Tuser,
+        Tboolean,
+        Tchar,
+        Tint,
+        Tvoid
+    }
+    class SymbolTable
+    {
+        public static BuiltInTypeSymbol _boolean = new BuiltInTypeSymbol("boolean", SymbolType.Tboolean);
+        public static BuiltInTypeSymbol _char = new BuiltInTypeSymbol("char", SymbolType.Tchar);
+        public static BuiltInTypeSymbol _int = new BuiltInTypeSymbol("int", SymbolType.Tint);
+        public static BuiltInTypeSymbol _void = new BuiltInTypeSymbol("void", SymbolType.Tvoid);
+
+        GlobalScope globals = new GlobalScope();
+
+        protected void initTypeSystem()
+        {
+            globals.define(_boolean);
+            globals.define(_char);
+            globals.define(_int);
+            globals.define(_void);
+        }
+
+        public SymbolTable()
+        {
+            initTypeSystem();
+        }
+
+        public override string ToString()
+        {
+            return globals.ToString();
+        }
+    }
+}
