20dbb828387f5f5074e0603879abae1c411be1bb
Paulo Chang <pigenio@gmail.com>
2014-06-09 11:48:28 -0600

    Finished Compiler Implementation

---
+++ DecafIde/HelperClasses/TypeConstructor.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DecafIde.HelperClasses
+{
+    /// <summary>
+    /// Gets the MSIL type mapping
+    /// </summary>
+    static class TypeConstructor
+    {
+        public static string getMappedType(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    return "int32";
+                case "boolean":
+                    return "bool";
+                default:
+                    return type;
+            }
+        }
+    }
+}
